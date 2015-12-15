6f9ff9944259a3df77ab1df64cb9a80ba5010e4c
LiquidSmokeX64 <liquidsmokex64@gmail.com>
2015-12-15 02:18:36 -0800

    Odexing

--- Android.mk
+++ Android.mk
@@ -5,4 +5,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files) \
 
 LOCAL_PACKAGE_NAME := SpeechRecorder
 
+# Enable dexpreopt to speed boot time
+WITH_DEXPREOPT := true
+
 include $(BUILD_PACKAGE)
